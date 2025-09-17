176e30790bd358a4b11839fecb29a15a37b9f8c0
KhadidjaMAHMOUD <khadidja.mahmoud@groupe-esigelec.org>
2025-09-17 15:37:46 +0200

    initial commi

---
+++ src/main/test/java/com/bibliotheque/service/LibraryServiceTest.java
@@ -0,0 +1,13 @@
+package com.bibliotheque.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryServiceTest {
+
+    @Test
+    void additionFonctionne() {
+        assertEquals(3, 1 + 2, "1+2 devrait valoir 3");
+    }
+}
+ 
